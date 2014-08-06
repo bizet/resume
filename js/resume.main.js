69429b83baf8f87d18540a85dcfd7ae4d74d2c3b
bizet <bizet.cn@hotmail.com>
2014-08-06 23:11:06 +0800

    ci

--- js/resume.main.js
+++ js/resume.main.js
@@ -5,27 +5,36 @@ require(['requirejs.config'] , function() {
         'model/animate.model'
       ], function(Animate) {
           var init_win_width = $(window).width();
-          var basic_info = new Animate({
+          var sections = [];
+          sections.push(new Animate({
             elem: $('#section-basic-info'),
             animate_css: 'section-basic-info',
             scroll_pos: 0
-          });
-          var brief_intro = new Animate({
+          }));
+          sections.push(new Animate({
             elem: $('#section-brief-intro'),
             animate_css: 'section-brief-intro',
-            scroll_pos: 20
-          });
+            scroll_pos: 100
+          }));
+          sections.push(new Animate({
+            elem: $('#section-alu'),
+            animate_css: 'section-alu',
+            scroll_pos: 400
+          }));
           $(window).scroll(function() {
-            basic_info.run($(window).scrollTop());
-            brief_intro.run($(window).scrollTop());
+            var pos = $(window).scrollTop();
+            $(sections).each(function() {
+              this.run(pos);
+            });
           })
           $(window).resize(function() {
             var width = $(window).width();
             if (width < 400) width = 400;
             if (width > 1200) width = 1200;
             $('body').css('width', width * 0.8);
-            basic_info.resize(width);
-            brief_intro.resize(width);
+            $(sections).each(function() {
+              this.resize(width);
+            });
           })
           $(window).scroll();
           $(window).resize();
