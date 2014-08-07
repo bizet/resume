f7ca8482791fac9af7d0cadc8d96e20c6a466aea
bizet <bizet.cn@hotmail.com>
2014-08-07 16:53:35 +0800

    ci

--- js/model/animate.model.js
+++ js/model/animate.model.js
@@ -1,15 +1,23 @@
 
 define(['jquery'], function($) {
+  var in_view = function(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      (rect.top + rect.height/2) >= 0 && 
+      rect.bottom <= ($(window).height() + rect.height/2)
+    )
+  };
   function User(opt) {
     this.elem = opt.elem;
     this.animate_css = opt.animate_css;
-    this.scroll_pos = opt.scroll_pos;
+    //this.scroll_pos = opt.scroll_pos;
     this.done = false;
   };
   User.prototype =  {
-    run: function(pos) {
+    scroll: function() {
       if (!this.done) {
-        if (pos >= this.scroll_pos) {
+        //if (pos >= this.scroll_pos) {
+        if (in_view(this.elem[0])) {
           this.elem.addClass(this.animate_css);
           this.done = true;
         }
--- js/resume.main.js
+++ js/resume.main.js
@@ -9,22 +9,22 @@ require(['requirejs.config'] , function() {
           sections.push(new Animate({
             elem: $('#section-basic-info'),
             animate_css: 'section-basic-info',
-            scroll_pos: 0
           }));
           sections.push(new Animate({
             elem: $('#section-brief-intro'),
             animate_css: 'section-brief-intro',
-            scroll_pos: 100
           }));
           sections.push(new Animate({
             elem: $('#section-alu'),
             animate_css: 'section-alu',
-            scroll_pos: 400
+          }));
+          sections.push(new Animate({
+            elem: $('#section-huawei'),
+            animate_css: 'section-huawei',
           }));
           $(window).scroll(function() {
-            var pos = $(window).scrollTop();
             $(sections).each(function() {
-              this.run(pos);
+              this.scroll();
             });
           })
           $(window).resize(function() {
@@ -34,9 +34,9 @@ require(['requirejs.config'] , function() {
             $('body').css('width', width * 0.8);
             $(sections).each(function() {
               this.resize(width);
+              this.scroll();
             });
           })
-          $(window).scroll();
           $(window).resize();
       });
     });
